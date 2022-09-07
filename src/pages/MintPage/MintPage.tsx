25150509cd2665b608725fd15f734bb38a57afba
sowell-owen <karakulin.toshik@gmail.com>
2022-09-07 16:07:52 +0300

    feat: integrated addFund func

--- src/pages/MintPage/MintPage.tsx
+++ src/pages/MintPage/MintPage.tsx
@@ -1,20 +1,47 @@
-import React, { useState } from 'react';
+import { BigNumber, ethers } from 'ethers';
+import React, { useEffect, useState } from 'react';
+
+import { CRYPTO_BUGGY_ADDRESS } from '../../helper/constants';
+import { CryptoBuggy__factory } from '../../typechain';
 import './mintPage.scss';
+declare let window: any;
 
 function MintPage() {
   const [amountToDonate, setAmountToDonate] = useState('0');
+  const [buggyPrice, setBuggyPrice] = useState<BigNumber>(BigNumber.from(0));
   const [isError, setIsError] = useState(false);
 
-  const donate = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const cryptoBuggyContract = CryptoBuggy__factory.connect(CRYPTO_BUGGY_ADDRESS, signer);
+
+  const addFund = async () => {
     if (+amountToDonate <= 0 || +amountToDonate >= 2000) {
       setIsError(true);
       return;
     }
-
-    console.log(amountToDonate);
+    try {
+      const addFunxTx = await cryptoBuggyContract.addFund('test signature', { value: buggyPrice });
+      await addFunxTx.wait();
+      console.log('Funds sended');
+    } catch (e) {
+      console.log(e);
+    }
 
     setIsError(false);
   };
+
+  useEffect(() => {
+    const getBuggyPrice = async () => {
+      const result = await cryptoBuggyContract.price();
+      return result;
+    };
+    getBuggyPrice()
+      .then(res => {
+        console.log('Price of 1 buggy: ', ethers.utils.formatUnits(res.toString()));
+        setBuggyPrice(res);
+      });
+  }, []);
 
   return (
     <div>
@@ -29,8 +56,9 @@ function MintPage() {
           onChange={(event) => setAmountToDonate(event.target.value)}
         ></input>
         {isError && <span>Amount in input is not correct</span>}
-        <button onClick={() => donate()}>Donate</button>
+        <button onClick={() => addFund()}>Donate</button>
       </div>
+      <p>Price of 1 buggy: {ethers.utils.formatUnits(buggyPrice.toString())}</p>
     </div>
   );
 }
