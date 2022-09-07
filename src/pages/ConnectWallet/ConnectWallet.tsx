eb5e250d6e39ac46ebdfdf2161a0a3872ba854f1
sowell-owen <karakulin.toshik@gmail.com>
2022-09-07 14:56:30 +0300

    feat: metamask connection

--- src/pages/ConnectWallet/ConnectWallet.tsx
+++ src/pages/ConnectWallet/ConnectWallet.tsx
@@ -3,12 +3,20 @@ import React from 'react';
 import { useState } from 'react';
 import './connectWallet.scss';
 
-import { uauth, gnosisconnect } from '../../helper/connectors';
+import { uauth, gnosisconnect, injected } from '../../helper/connectors';
 
 const ConnectWallet = () => {
   const { activate, deactivate, account } = useWeb3React();
   const [user, setUser] = useState<string>();
   const [isGnosisError, setGnosisError] = useState(false);
+
+  async function connectMetamask() {
+    try {
+      await activate(injected);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   async function connectGnosis() {
     await activate(gnosisconnect);
@@ -19,7 +27,7 @@ const ConnectWallet = () => {
     } else {
       setGnosisError(false);
     }
-  }
+  };
 
   async function handleLogin() {
     await activate(uauth);
@@ -29,20 +37,21 @@ const ConnectWallet = () => {
     if (userObj.value) {
       setUser(userObj.value);
     }
-  }
+  };
 
   async function handleLogout() {
     deactivate();
     setUser('');
     setGnosisError(false);
-  }
+  };
 
   return (
     <div className="container">
       {user && <span>Connected user: {user}</span>}
       {account && !user && <span>Connected account: {account}</span>}
-      <button onClick={handleLogin}>ConnectWallet</button>
+      <button onClick={handleLogin}>Connect via UD</button>
       <button onClick={connectGnosis}>Connect Gnosis</button>
+      <button onClick={connectMetamask}>Connect Metamask</button>
       <button onClick={handleLogout}>Disconnect wallet</button>
       {isGnosisError && <span>The app is loaded outside safe context</span>}
     </div>
