b2f8454e06c6b598473166b160e0898a899f773f
Andrii <shvaika.andrii@gmail.com>
2022-10-05 13:33:01 +0300

    removed console log

--- src/components/NFT/NFT.tsx
+++ src/components/NFT/NFT.tsx
@@ -2,8 +2,6 @@ import './NFT.scss';
 import React from 'react';
 
 function NFT({ image }: { image: string | undefined }) {
-  console.log('Item image: ', image);
-
   return (
     <div className="nft__container">
       <div className="nft__container-img">
