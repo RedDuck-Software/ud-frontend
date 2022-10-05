87cf1898a04cf06c760c3ba1e8686cbd23bfe101
Andrii <shvaika.andrii@gmail.com>
2022-10-05 13:30:57 +0300

    fixed image displaying

--- src/components/NFT/NFT.tsx
+++ src/components/NFT/NFT.tsx
@@ -2,14 +2,12 @@ import './NFT.scss';
 import React from 'react';
 
 function NFT({ image }: { image: string | undefined }) {
+  console.log('Item image: ', image);
+
   return (
     <div className="nft__container">
       <div className="nft__container-img">
-        <img
-          className="nft__image"
-          src={image && `https://ipfs.io/ipfs/${image}`}
-          alt=""
-        />
+        <img className="nft__image" src={image ? image : ''} alt="" />
       </div>
     </div>
   );
